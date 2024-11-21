705d218c45ad4983f3e28585422711571d97daf3
NguyenDanPhuong <nguyendanphuong@live.com>
2024-11-21 13:35:04 +0700

    Success to run system test with headless Chrome using cuprite

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -4,4 +4,16 @@ class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save article without title" do
+    article = Article.new
+    assert_not article.save, "Saved the article without a title"
+  end
+
+  test "should report error" do
+    assert_raises(NameError) do
+      some_undefined_variable
+    end
+    assert true
+  end
 end
