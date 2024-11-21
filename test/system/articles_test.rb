4b643615def9d3d53e29402a71a82c4aaa90cd40
NguyenDanPhuong <nguyendanphuong@live.com>
2024-11-21 14:39:20 +0700

    Done system tests

--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -5,4 +5,13 @@ class ArticlesTest < ApplicationSystemTestCase
     visit articles_url
     assert_selector "h1", text: "Articles"
   end
+
+  test "creating a Article" do
+    visit articles_url
+    click_on "New Article"
+    fill_in "Title", with: "My Title 2024"
+    fill_in "Body", with: "Body with 20 characters"
+    click_on "Create Article"
+    assert_text "My Title 2024"
+  end
 end
